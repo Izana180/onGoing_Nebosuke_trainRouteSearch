8e9db19a5f26c0bc8933b646fd2cd88daa9d49f6
Izana180 <valleymouth0402@yahoo.co.jp>
2025-03-25 14:04:27 +0900

    入力枠の定義

--- lib/presentation/parts/input/custom_basic_input.dart
+++ lib/presentation/parts/input/custom_basic_input.dart
@@ -37,7 +37,7 @@ class CustomBasicInput extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final textCounter = useState(textEditingController?.text.length ?? 0);
-    final OutlineInputBorder = errText == null
+    final outlineInputBorder = errText == null
         // エラーでない
         ? const OutlineInputBorder(
           borderSide: BorderSide(color: Colors.transparent),
@@ -48,5 +48,97 @@ class CustomBasicInput extends HookConsumerWidget {
           borderSide: BorderSide(color: CustomColors.strongRed),
           borderRadius: BorderRadius.all(Radius.circular(8)),
         );
+    return Column(
+      children: [
+        // いずれかが存在するときのみ表示箇所のRowを生成
+        if(errText!=null || subText != null || labelText != null)
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              if(labelText != null)
+                Container(
+                  alignment: Alignment.centerLeft,
+                  margin: EdgeInsets.only(right: 5),
+                  padding: const EdgeInsets.only(bottom: 4),
+                  child: Text(
+                    labelText ?? '',
+                    style: const TextStyle(
+                      color: CustomColors.baseWhite,
+                      fontSize: 13,
+                    ),
+                  ),
+                ),
+              Builder(builder: (context) {
+                if(errText != null) {
+                  return Container(
+                    alignment: Alignment.centerLeft,
+                    padding: const EdgeInsets.only(bottom: 4),
+                    child: Text(
+                      errText!,
+                      style: const TextStyle(color: CustomColors.strongRed, fontSize: 10),
+                    ),
+                  );
+                } else if (subText != null) {
+                  return Container(
+                    alignment: Alignment.centerLeft,
+                    padding: const EdgeInsets.only(bottom: 4),
+                    child: Text(
+                      subText!,
+                      style: TextStyle(color: subTextColor ?? CustomColors.baseWhite, fontSize: 10),
+                    ),
+                  );
+                } else {
+                  return Container();
+                }
+              }),
+            ],
+          ),
+        TextField(
+          maxLines: maxLine ?? 1,
+          focusNode: focusNode,
+          controller: textEditingController,
+          obscureText: isPassword,
+          decoration: InputDecoration(
+            fillColor: isUseThinBlack ? CustomColors.minBlack : CustomColors.inputBlack,
+            filled: true,
+            hintText: hintText ?? '入力してください',
+            hintStyle: const TextStyle(
+              color: CustomColors.baseGray3,
+              fontSize: 14,
+            ),
+            enabledBorder: outlineInputBorder,
+            border: outlineInputBorder,
+            focusedBorder: outlineInputBorder,
+            disabledBorder: outlineInputBorder,
+            focusedErrorBorder: outlineInputBorder,
+            errorBorder: outlineInputBorder,
+          ),
+          style: const TextStyle(
+            color: CustomColors.baseWhite,
+            fontSize: 14,
+          ),
+          onChanged: (value) {
+            if(onChanged != null) {
+              onChanged!(value);
+            }
+            textCounter.value = value.length;
+          },
+        ),
+        if(showTextCounter)
+          HookBuilder(builder: (context) {
+            return Container(
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.only(top: 4),
+              child: Text(
+                '${textCounter.value}文字',
+                style: const TextStyle(
+                  color: CustomColors.baseWhite,
+                  fontSize: 12,
+                ),
+              ),
+            );
+          }),
+      ],
+    );
   }
 }
