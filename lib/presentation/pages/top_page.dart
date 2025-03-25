dde6056ce13ac9deb3cd695c8f103254156bee7c
Izana180 <valleymouth0402@yahoo.co.jp>
2025-03-26 01:27:09 +0900

    年月日の選択リストを追加

--- lib/presentation/pages/top_page.dart
+++ lib/presentation/pages/top_page.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:nebosuke_trainroutesearch/domain/top_page_domain.dart';
 import 'package:nebosuke_trainroutesearch/presentation/parts/input/custom_basic_input.dart';
+import 'package:nebosuke_trainroutesearch/presentation/parts/input/custom_select_box.dart';
 
 class TopPage extends HookConsumerWidget {
+  // 年月日の選択肢を生成
+  List<CustomSelectItem> selectYear = List.generate(
+    2025 - 2020 + 1,  // 要素数
+    (index) {
+      int year = 2020 + index;
+      return CustomSelectItem<int>(value: year, label: '$year年');
+    },
+  );
+  List<CustomSelectItem> selectMonth = List.generate(
+    12,
+    (index) {
+      int month = index+1;
+      return CustomSelectItem<int>(value: month, label: '$month月');
+    },
+  );
+  List<CustomSelectItem> selectDay = List.generate(
+    31,
+    (index) {
+      int day = index+1;
+      return CustomSelectItem<int>(value: day, label: '$day日');
+    },
+  );
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final screenWidth = MediaQuery.of(context).size.width;
@@ -31,6 +56,38 @@ class TopPage extends HookConsumerWidget {
               hintText: '到着駅を入力',
               textEditingController: arrivalController
             ),
+            SizedBox(height: 12),
+            // 年月日を横並びに表示
+            // TODO:今日の日付をデフォルト表示するか、現在時刻に設定するボタンを追加
+            Row(
+              children: [
+                SizedBox(
+                  width: screenWidth * 0.3,
+                  child: CustomSelectBox<int>(
+                    value: selectYearState,
+                    hintText: '年',
+                    items: selectYear,
+                  ),
+                ),
+                const SizedBox(width: 10),
+                SizedBox(
+                  width: screenWidth * 0.2,
+                  child: CustomSelectBox<int>(
+                    value: selectMonthState,
+                    hintText: '月',
+                    items: selectMonth,
+                  ),
+                ),
+                const SizedBox(width: 10),
+                SizedBox(
+                  width: screenWidth * 0.2,
+                  child: CustomSelectBox<int>(
+                    value: selectDayState,
+                    hintText: '日',
+                    items: selectDay,
+                  ),
+                )
+            ],)
           ],
         ),
       ),
