6926d01ff543c07efefca0124d0f0c378cdcd4f4
Izana180 <valleymouth0402@yahoo.co.jp>
2025-03-25 16:58:27 +0900

    デフォルト背景色を指定

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:nebosuke_trainroutesearch/presentation/style/custom_colors.dart';
 import 'presentation/pages/top_page.dart';
+import 'package:nebosuke_trainroutesearch/presentation/parts/input/custom_basic_input.dart';
 
 class MyApp extends StatelessWidget {
   @override
@@ -7,6 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'ねぼすけあんない',
       theme: ThemeData(
+        scaffoldBackgroundColor: CustomColors.blackBackground,
         primarySwatch: Colors.green,
       ),
       home: TopPage(),
