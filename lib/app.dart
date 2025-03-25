dbcf82946ec9b1a96a445a9bf9907693c1676cc5
Izana180 <valleymouth0402@yahoo.co.jp>
2025-03-26 01:23:35 +0900

    不要なインポートを削除

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nebosuke_trainroutesearch/presentation/style/custom_colors.dart';
 import 'presentation/pages/top_page.dart';
-import 'package:nebosuke_trainroutesearch/presentation/parts/input/custom_basic_input.dart';
 
 class MyApp extends StatelessWidget {
   @override
