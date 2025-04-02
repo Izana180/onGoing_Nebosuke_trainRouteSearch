2566f2d618a55dbc447f0b8bae31bf7293685aab
Izana180 <valleymouth0402@yahoo.co.jp>
2025-04-03 03:15:17 +0900

    時間のproviderを追加

--- lib/domain/top_page_domain.dart
+++ lib/domain/top_page_domain.dart
@@ -3,3 +3,5 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 final selectYearState = StateProvider<int?>((ref) => null);
 final selectMonthState = StateProvider<int?>((ref) => null);
 final selectDayState = StateProvider<int?>((ref) => null);
+final selectHourState = StateProvider<int?>((ref) => null);
+final selectMinuteState = StateProvider<int?>((ref) => null);
